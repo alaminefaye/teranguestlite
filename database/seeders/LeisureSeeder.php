e24674d54448e15abbc0e7ac178377099a890848
Al AMINE FAYE <Zhuanz@MacBook-Pro.local>
2026-02-15 12:00:43 +0000

    changement

--- database/seeders/LeisureSeeder.php
+++ database/seeders/LeisureSeeder.php
@@ -43,13 +43,18 @@ class LeisureSeeder extends Seeder
                 ]
             );
 
+            // Sports les plus courants dans les grands hôtels (ordre d'affichage)
             $sportChildren = [
                 ['name' => 'Golf', 'description' => 'Réservation Tee-time et location de matériel golf.', 'type' => 'golf', 'display_order' => 0],
                 ['name' => 'Tennis', 'description' => 'Réservation de courts et location de matériel tennis.', 'type' => 'tennis', 'display_order' => 1],
-                ['name' => 'Sport & Fitness', 'description' => 'Horaires de la salle et réservation de coach personnel.', 'type' => 'fitness', 'display_order' => 2],
-                ['name' => 'Piscine', 'description' => 'Accès piscine et créneaux nage.', 'type' => 'other', 'display_order' => 3],
-                ['name' => 'Yoga & Pilates', 'description' => 'Cours et séances yoga, pilates.', 'type' => 'other', 'display_order' => 4],
-                ['name' => 'Running & VTT', 'description' => 'Parcours running et VTT, location de matériel.', 'type' => 'other', 'display_order' => 5],
+                ['name' => 'Squash', 'description' => 'Réservation du court de squash et location de raquettes.', 'type' => 'other', 'display_order' => 2],
+                ['name' => 'Sport & Fitness', 'description' => 'Horaires de la salle et réservation de coach personnel.', 'type' => 'fitness', 'display_order' => 3],
+                ['name' => 'Piscine', 'description' => 'Accès piscine, créneaux nage et horaires.', 'type' => 'other', 'display_order' => 4],
+                ['name' => 'Aquagym & Natation', 'description' => 'Cours d\'aquagym et cours de natation.', 'type' => 'other', 'display_order' => 5],
+                ['name' => 'Yoga & Pilates', 'description' => 'Cours et séances yoga, pilates.', 'type' => 'other', 'display_order' => 6],
+                ['name' => 'Running & VTT', 'description' => 'Parcours running et VTT, location de matériel.', 'type' => 'other', 'display_order' => 7],
+                ['name' => 'Beach Volley', 'description' => 'Réservation du terrain et créneaux beach volley.', 'type' => 'other', 'display_order' => 8],
+                ['name' => 'Cours collectifs', 'description' => 'Cours de groupe : stretching, cardio, renforcement.', 'type' => 'other', 'display_order' => 9],
             ];
             foreach ($sportChildren as $data) {
                 LeisureCategory::withoutGlobalScope('enterprise')->firstOrCreate(
@@ -66,8 +71,11 @@ class LeisureSeeder extends Seeder
                 );
             }
 
+            // Loisirs typiques grands hôtels
             $loisirsChildren = [
                 ['name' => 'Spa & Bien-être', 'description' => 'Carte des soins et réservation des créneaux de massage.', 'type' => 'spa', 'display_order' => 0],
+                ['name' => 'Hammam & Sauna', 'description' => 'Accès hammam et sauna, horaires et réservation.', 'type' => 'other', 'display_order' => 1],
+                ['name' => 'Excursions & Découverte', 'description' => 'Activités et excursions proposées par l\'hôtel.', 'type' => 'other', 'display_order' => 2],
             ];
             foreach ($loisirsChildren as $data) {
                 LeisureCategory::withoutGlobalScope('enterprise')->firstOrCreate(
