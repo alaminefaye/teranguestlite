4d1eaba7591f5bd38bb6445ffb48a527687a899f
Al AMINE FAYE <Zhuanz@MacBook-Pro.local>
2026-03-05 22:02:48 +0000

    correction champ client rrservation

--- public/client/flutter_bootstrap.js
+++ public/client/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "94734745" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "924135570" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
